d06c4fbb20bce5124bdb1451292a5b3dd573858e
dial1201 <fancy_95310hotmail.com>
2021-07-08 15:51:30 +0200

    :sparkles: base models, fixes #1

--- src/nurse/models.py
+++ src/nurse/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    firstname = models.CharField(max_length=30, blank=False)
+    lastname = models.CharField(max_length=30, blank=False)
+    address = models.CharField(max_length=300, blank=False)
+    zip_code = models.CharField(blank=False, max_length=5)
+    city = models.CharField(max_length=300, blank=False)
+    phone = models.CharField(blank=False, max_length=30)
+
+
+class Nurse(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    phone = models.IntegerField(blank=False)
+    address = models.CharField(max_length=300, blank=False)
+    zip_code = models.CharField(max_length=5, blank=False)
+    city = models.CharField(max_length=300, blank=False)
+    patients = models.ManyToManyField(Patient)
+
+
+class Prescription(models.Model):
+    carte_vitale = models.IntegerField()
+    caisse_rattachement = models.IntegerField()
+    prescribing_doctor = models.CharField(max_length=300, blank=False)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False)
+    at_renew = models.BooleanField(blank=False)
+    photo_prescription = models.CharField(max_length=300, blank=False)
+    patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
