23b205597aed6d05ced02fe063490360112ad9f3
dial1201 <fancy_95310hotmail.com>
2021-07-12 17:18:32 +0200

    :construction: start apirest

--- src/nurse/views.py
+++ src/nurse/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from nurse.models import Patient, Prescription
+from nurse.serializers import PatientSerializer, PrescriptionSerializer
+from rest_framework import viewsets
+
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+class PrescriptionViewSet(viewsets.ModelViewSet):
+    queryset = Prescription.objects.all()
+    serializer_class = PrescriptionSerializer
