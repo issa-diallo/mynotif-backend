f47e9e8c4a696a56e122b92a03db9c4bc296380d
Andre Miras <andre.miras@gmail.com>
2022-09-11 11:39:32 +0200

    :break: Prepare terraform backend, refs #62
    `terraform/backend.tf` was generated, refs:
    https://github.com/cloudposse/terraform-aws-tfstate-backend
    
    Note that the `mynotif-deployment-automation-role` is managed at higher
    level in the `mynotif-infra` repository.
    
    Follow up pull request should setup the prescription bucket

---
+++ terraform/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "eu-west-3"
+    bucket         = "mynotif-backend-terraform-state"
+    key            = "terraform.tfstate"
+    dynamodb_table = "mynotif-backend-terraform-state-lock"
+    profile        = ""
+    role_arn       = ""
+    encrypt        = "true"
+  }
+}
---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.18"
+    }
+  }
+
+  required_version = ">=0.14.9"
+}
+
+provider "aws" {
+  region = var.aws_region
+  # Role to assume for AWS operations
+  assume_role {
+    role_arn = "arn:aws:iam::332944743618:role/mynotif-deployment-automation-role"
+  }
+}
+
+
+# You cannot create a new backend by simply defining this and then
+# immediately proceeding to "terraform apply". The S3 backend must
+# be bootstrapped according to the simple yet essential procedure in
+# https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
+module "terraform_state_backend" {
+  source = "cloudposse/tfstate-backend/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  version    = "0.38.1"
+  namespace  = var.app_name
+  name       = "terraform"
+  attributes = ["state"]
+
+  terraform_backend_config_file_path = "."
+  terraform_backend_config_file_name = "backend.tf"
+  force_destroy                      = false
+}
