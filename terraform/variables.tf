576e590eb178e041d40ce21787342a7e57eaf4ae
Issa <issa.diallo@lemonlearning.fr>
2023-06-26 15:21:15 +0200

    :wrench: add url netifly

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -61,6 +61,7 @@ variable "env_cors_allowed_origins" {
     "http://localhost:8000",
     "https://mynotif.herokuapp.com",
     "https://mynotif.vercel.app",
+    "https://delicate-pie-15d2c7.netlify.app",
   ]
 }
 
