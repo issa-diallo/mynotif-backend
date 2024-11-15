3acefbed42625d9d41ea55121887d62f1adc64a8
Issa <issa.djehon@gmail.com>
2024-11-15 19:33:18 +0100

    :sparkles: Add subscription API views and serializer for Stripe integration
    - Added SubscriptionSerializer to handle subscription data.
    - Implemented SubscriptionViewSet for managing subscriptions via Stripe.
    - Created SubscriptionSuccessView and SubscriptionCancelView for
    handling Stripe payment success and cancellation responses.
    
    - Registered the subscription endpoints in the URLs.
    - Implemented tests for subscription creation, retrieval, success, and cancel views.

--- src/nurse/views.py
+++ src/nurse/views.py
@@ -1,7 +1,9 @@
+import stripe
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
+from django.urls import reverse
 from rest_framework import generics, mixins, status, viewsets
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny, IsAdminUser
@@ -9,13 +11,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from nurse.management.commands._notifications import notify
-from nurse.models import Nurse, Patient, Prescription, UserOneSignalProfile
+from nurse.models import (
+    Nurse,
+    Patient,
+    Prescription,
+    StripeProduct,
+    Subscription,
+    UserOneSignalProfile,
+)
 from nurse.serializers import (
     ExpandedPrescriptionSerializer,
     NurseSerializer,
     PatientSerializer,
     PrescriptionEmailSerializer,
     PrescriptionFileSerializer,
+    SubscriptionSerializer,
     UserOneSignalProfileSerializer,
     UserSerializer,
     UserSerializerV2,
@@ -257,3 +267,74 @@ class AdminNotificationView(APIView):
     def post(self, request):
         notify()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        plan = request.data.get("plan")  # "monthly" ou "annual"
+
+        try:
+            product = StripeProduct.objects.get(name="Essentiel")
+            price_id = (
+                product.monthly_price_id
+                if plan == "monthly"
+                else product.annual_price_id
+            )
+        except StripeProduct.DoesNotExist:
+            return Response({"error": "Product not found"}, status=404)
+
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types=["card"],
+                customer_email=user.email,
+                line_items=[{"price": price_id, "quantity": 1}],
+                mode="subscription",
+                success_url=request.build_absolute_uri(
+                    reverse("v1:subscription-success")
+                ),
+                cancel_url=request.build_absolute_uri(
+                    reverse("v1:subscription-cancel")
+                ),
+            )
+
+            serializer = self.get_serializer(
+                data={**request.data, "stripe_subscription_id": checkout_session.id}
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save(user=user, stripe_subscription_id=checkout_session.id)
+
+            headers = self.get_success_headers(serializer.data)
+            return Response(
+                {"sessionId": checkout_session.id},
+                status=status.HTTP_201_CREATED,
+                headers=headers,
+            )
+        except Exception as e:
+            print("Error during session creation:", e)
+            return Response({"error": str(e)}, status=500)
+
+    def retrieve(self, request, *args, **kwargs):
+        obj = get_object_or_404(self.get_queryset(), user=request.user)
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data)
+
+
+class SubscriptionSuccessView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {"message": "Subscription successful"}, status=status.HTTP_200_OK
+        )
+
+
+class SubscriptionCancelView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {"message": "Subscription cancelled"}, status=status.HTTP_200_OK
+        )
--- src/tests/nurse/test_views.py
+++ src/tests/nurse/test_views.py
@@ -1,7 +1,7 @@
 from datetime import date
 from pathlib import Path
 from unittest import mock
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import boto3
 import pytest
@@ -16,7 +16,14 @@ from moto import mock_aws
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from nurse.models import Nurse, Patient, Prescription, UserOneSignalProfile
+from nurse.models import (
+    Nurse,
+    Patient,
+    Prescription,
+    StripeProduct,
+    Subscription,
+    UserOneSignalProfile,
+)
 
 PASSWORD = "password1"
 FIRSTNAME = "John"
@@ -1132,3 +1139,89 @@ class TestUserOneSignalProfileView:
         """The endpoint should be under authentication."""
         response = APIClient().get(self.url)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+class TestSubscriptionViewSet:
+
+    create_url = reverse_lazy("v1:subscription-list")
+
+    def retrieve_url(self, pk):
+        return reverse_lazy("v1:subscription-detail", kwargs={"pk": pk})
+
+    @patch("nurse.views.stripe.checkout.Session.create")
+    @patch("nurse.views.StripeProduct.objects.get")
+    def test_create_subscription_success(
+        self, mock_get_product, mock_create_session, client, user
+    ):
+        client.force_login(user)
+        data = {
+            "plan": "monthly",
+        }
+        mock_checkout_session = MagicMock()
+        mock_checkout_session.id = "session_id_example"
+        mock_checkout_session.url = "https://checkout.stripe.com/pay/example"
+        mock_create_session.return_value = mock_checkout_session
+        response = client.post(self.create_url, data, format="json")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["sessionId"] == "session_id_example"
+
+    @patch("nurse.views.stripe.checkout.Session.create")
+    @patch("nurse.views.StripeProduct.objects.get")
+    def test_create_subscription_product_not_found(
+        self, mock_get_product, mock_create_session, client, user
+    ):
+        client.force_login(user)
+        data = {"plan": "monthly"}
+        mock_get_product.side_effect = StripeProduct.DoesNotExist
+        response = client.post(self.create_url, data, format="json")
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert response.data["error"] == "Product not found"
+
+    @patch("nurse.views.stripe.checkout.Session.create")
+    @patch("nurse.views.StripeProduct.objects.get")
+    def test_create_subscription_stripe_session_creation_failed(
+        self, mock_get_product, mock_create_session, client, user
+    ):
+        client.force_login(user)
+        data = {"plan": "monthly"}
+        mock_product = MagicMock()
+        mock_product.monthly_price_id = "price_monthly_id_example"
+        mock_product.annual_price_id = "price_annual_id_example"
+        mock_get_product.return_value = mock_product
+        mock_create_session.side_effect = Exception("Error during session creation")
+        response = client.post(self.create_url, data, format="json")
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert "Error during session creation" in response.data["error"]
+
+    @patch("nurse.views.stripe.checkout.Session.create")
+    @patch("nurse.views.StripeProduct.objects.get")
+    def test_retrieve_subscription_success(
+        self, mock_get_product, mock_create_session, client, user
+    ):
+        client.force_login(user)
+        subscription = Subscription.objects.create(
+            user=user, stripe_subscription_id="session_id_example"
+        )
+        response = client.get(self.retrieve_url(subscription.pk), format="json")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["user"] == user.id
+        assert response.data["stripe_subscription_id"] == "session_id_example"
+        assert response.data["is_active"] is True
+
+    def test_retrieve_subscription_not_found(self, client, user):
+        client.force_login(user)
+        response = client.get(self.retrieve_url(9999), format="json")
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_subscription_success_view(self, client):
+        response = client.get("/api/v1/subscriptions/success/")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["message"] == "Subscription successful"
+
+    def test_subscription_cancel_view(self, client):
+        response = client.get("/api/v1/subscriptions/cancel/")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["message"] == "Subscription cancelled"
